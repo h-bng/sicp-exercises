7305c7731f56b56497162ac0c8942e7c8f6a8d18
H. Bingol <h.bingol@pm.me>
2021-01-27 17:15:28 +0300

    rewritten in block structure

--- x-nth-power.scm
+++ x-nth-power.scm
@@ -1,21 +1,19 @@
-(define (power-iter x n p)
-  (if (= n 0)
-      p
-      (power-iter x (- n 1) (* p x))))
-
 (define (power x n)
-  (power-iter x n 1))
+  (define (power-iter n result)
+    (if (= n 0)
+      result
+      (power-iter (- n 1) (* result x))))
+  (power-iter n 1))
 
-(define (improve-n x y n)
-  (/ (+ (* x (- n 1)) (/ y (power x (- n 1)))) n))
-
-(define (good-enough-n? x y n)
-  (< (abs (- (power x n) y)) 0.00000001))
-
-(define (n-iter x y n)
-  (if (good-enough-n? x y n)
-      x
-      (n-iter (improve-n x y n) y n)))
-
-(define (x-over-n n y)
-  (n-iter 1.0 y n))
+(define (solve-expon n y)
+  (define (abs x)
+    ((if (< x 0) - +) x))
+  (define (improve x)
+    (/ (+ (* x (- n 1)) (/ y (power x (- n 1)))) n))
+  (define (good-enough? x)
+    (< (abs (- (power x n) y)) 0.00000001))
+  (define (iter x)
+    (if (good-enough? x)
+	x
+	(iter (improve x))))
+  (iter 1.0))
